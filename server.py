4532aa008dd93fd110d197d6b5a05a90d2d93f53
Hashmi_Abdullah <abdullahhashmi005@gmail.com>
2023-03-07 04:07:56 +0500

    Update server.py
    Server file updated

--- server.py
+++ server.py
@@ -17,7 +17,8 @@ def get_eval_fn(model: LogisticRegression):
     """Return an evaluation function for server-side evaluation."""
 
     # Load test data here to avoid the overhead of doing it in `evaluate` itself
-    _, (X_test, y_test) = utils.load_data()
+    _, (X_test, y_test) = utils.load_data_client1()
+    #_, (X_test1, y_test1) = utils.load_data_client2()
 
     # The `evaluate` function will be called after every round
     def evaluate(server_round, parameters: fl.common.NDArrays, config):
@@ -25,6 +26,8 @@ def get_eval_fn(model: LogisticRegression):
         utils.set_model_params(model, parameters)
         loss = log_loss(y_test, model.predict_proba(X_test))
         accuracy = model.score(X_test, y_test)
+        #loss1 = log_loss(y_test, model.predict_proba(X_test))
+        #accuracy1 = model.score(X_test1, y_test1)
         return loss, {"accuracy": accuracy}
 
     return evaluate
